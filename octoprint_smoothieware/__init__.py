0d87fd5267a8e849e16acb9e1b57077814da413d
Randy C. Will <randall.will@gmail.com>
2018-03-21 06:34:46 +0000

    Config listener

--- octoprint_smoothieware/__init__.py
+++ octoprint_smoothieware/__init__.py
@@ -1,26 +1,28 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
 class SmoothiewarePlugin(octoprint.plugin.SettingsPlugin,
+                         octoprint.plugin.StartupPlugin,
                          octoprint.plugin.AssetPlugin,
                          octoprint.plugin.TemplatePlugin):
+
+        def on_after_startup(self):
+            self._logger.info("Expecting Smoothieware block device at " + self._settings.get(["device"]))
 
 	##~~ SettingsPlugin mixin
 
 	def get_settings_defaults(self):
 		return dict(
-			# put your plugin's default settings here
+                   device="/dev/sda1"
 		)
+
+        def get_template_configs(self):
+            return [
+                    dict(type="settings", custom_bindings="False")
+                    ]
+
 
 	##~~ AssetPlugin mixin
 
@@ -55,11 +57,10 @@ class SmoothiewarePlugin(octoprint.plugin.SettingsPlugin,
 			)
 		)
 
-
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-#__plugin_name__ = "Smoothieware Plugin"
+__plugin_name__ = "Smoothieware Integration"
 
 def __plugin_load__():
 	global __plugin_implementation__
--- octoprint_smoothieware/static/js/smoothieware.js
+++ octoprint_smoothieware/static/js/smoothieware.js
@@ -9,10 +9,35 @@ $(function() {
         var self = this;
 
         // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
+        self.loginStateViewModel = parameters[0];
+        self.settingsViewModel = parameters[1];
 
-        // TODO: Implement your plugin's view model here.
+	console.log(self.loginStateViewModel);
+	console.log(self.settingsViewModel);
+
+	self.loadingConfig = ko.observable(false);
+
+	var idle_count = 0;
+	var config = "";
+	self.fromCurrentData = function(data) {
+	   _.each(data.logs, function(line) {
+	      if(/# Smoothieboard configuration file/.exec(line)) {
+		 self.loadingConfig(true);
+	      }
+	      if(self.loadingConfig()) {
+		 if(/Recv:.*[a-z]{3,}/.exec(line)) {
+		    config += line + "\n";
+		    idle_count = 0;
+		 } else if(/Send:/.exec(line)) {
+		    if(++idle_count > 1) {
+		       self.loadingConfig(false);
+		       alert(config);
+		    }
+		 }
+	      }
+	      return line;
+	   });
+	}
     }
 
     /* view model class, parameters for constructor, container to bind to
@@ -22,8 +47,8 @@ $(function() {
     OCTOPRINT_VIEWMODELS.push({
         construct: SmoothiewareViewModel,
         // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        dependencies: [ "loginStateViewModel", "settingsViewModel" ],
         // Elements to bind to, e.g. #settings_plugin_smoothieware, #tab_plugin_smoothieware, ...
-        elements: [ /* ... */ ]
+        elements: [ "#settings_plugin_smoothieware" ]
     });
 });
